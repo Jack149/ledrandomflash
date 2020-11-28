5125449035b6e157ff47e4ce1257628c3313caa2
Jack149 <31501037+Jack149@users.noreply.github.com>
2020-11-27 22:15:52 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
 basic.forever(function () {
-	
+    while (true) {
+        led.plot(randint(0, 5), randint(0, 5))
+        led.setBrightness(randint(0, 255))
+        basic.pause(100)
+        led.unplot(0, 0)
+    }
 })
