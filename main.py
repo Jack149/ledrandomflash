cf1d2a4c8ad30ba920f45aa06613b6443ac359d4
Jack149 <31501037+Jack149@users.noreply.github.com>
2021-09-22 13:26:49 -0500

    Minor cleanup

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-basic.forever(function () {
-	
-})
+randX = 0
+randY = 0
+
+def on_button_pressed_a():
+    pass
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_forever():
+    global randX, randY
+    while True:
+        randX = randint(0, 5)
+        randY = randint(0, 5)
+        led.plot(randX, randY)
+        led.set_brightness(randint(0, 255))
+        basic.pause(100)
+        led.unplot(randX, randY)
+basic.forever(on_forever)
